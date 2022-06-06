89434bd50860ee48e33392adecfd738dd65a2edd
Mark Jumba <jumbamark@yahoo.com>
2022-06-06 20:33:43 +0300

    Added component that lists items

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ListItem from '../components/ListItem';
 
 const NotesListPage = () => {
     let [notes, setNotes] = useState([]);
@@ -8,7 +9,7 @@ const NotesListPage = () => {
     },[])
 
     let getNotes = async () => {
-        let response = await fetch("http://127.0.0.1:8000/api/notes/");
+        let response = await fetch("/api/notes/");
         let data = await response.json();
         console.log('DATA: ', data)
         setNotes(data)
@@ -18,7 +19,7 @@ const NotesListPage = () => {
         <div>
             <div className="notes-list">
                 {notes.map((note, index) => (
-                    <h3 key={index}>{note.body}</h3>
+                    <ListItem key={index} note={note}/>
                 ))}
             </div>
         </div>
