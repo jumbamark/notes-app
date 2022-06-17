a65a170de92ea0a3600686421cb5e2dcc37e3c86
Mark Jumba <jumbamark@yahoo.com>
2022-06-17 17:32:09 +0300

    making the API restful

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('notes/', views.getNotes, name='notes'),
-    path('notes/create/', views.createNote, name='create-note'),
-    path('notes/<int:pk>/update/', views.updateNote, name='update-note'),
-    path('notes/<int:pk>/delete/', views.deleteNote, name='delete-note'),
+    # path('notes/create/', views.createNote, name='create-note'),
+    # path('notes/<int:pk>/update/', views.updateNote, name='update-note'),
+    # path('notes/<int:pk>/delete/', views.deleteNote, name='delete-note'),
 
     path('notes/<int:pk>/', views.getNote, name='note'),
 ]
