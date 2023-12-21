d245e3d0628813f68c622e966036ca408c590279
Mark Jumba <jumbamark@yahoo.com>
2023-12-21 15:01:17 +0300

    Building out the HTML and styling

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 const ListItem = ({note}) => {
     return (
-        <div>
-            <h3>{note.body}</h3>
-        </div>
-    )
+        <Link to={`/note/${note.id}`}>
+            <div className="notes-list-item">
+                <h3>{note.body}</h3>
+            </div>
+        </Link>
+    );
 }
 
 export default ListItem
