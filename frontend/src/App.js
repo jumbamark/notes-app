8748fd391fbe7021e79011ad54ffadb0428c74ca
Mark Jumba <jumbamark@yahoo.com>
2023-12-21 15:01:17 +0300

    Added component that lists items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="App">
-      <Header/>
-      <NotesListPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<NotesListPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
